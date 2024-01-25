f1df7ad32062bc94d20e6fc18cfddd9b8a9adcba
Tathya Shukla <98167793+ts4949@users.noreply.github.com>
2024-01-25 17:24:49 +0530

    Added

---
+++ Practice/Practice1.2/Practice1.2.1.js
@@ -0,0 +1,20 @@
+//Create an object to store information about a person (e.g., name, age, address, etc.).
+// Write code to access and modify the object's properties.
+
+const person = {
+  name: 'Tathya',
+  age: 22,
+  city: 'Rajkot',
+};
+
+console.log(person.name); 
+console.log(person.age); 
+console.log(person.city); 
+
+person.name = 'Tathya Shukla';
+person.age = 23;
+person.city = 'Ahmedabad';
+
+console.log(person.name);
+console.log(person.age); 
+console.log(person.city); 
